ffe2bd46b32d406772e28bb8756709a2e7221e3d
yvromero <yvonne.romero@uds.edu.py>
2025-01-07 13:13:01 -0300

    first commit

---
+++ src/orders/dto/order-pagination.dto.ts
@@ -0,0 +1,13 @@
+import { PaginationDto } from "src/common";
+import { OrderStatusList } from "../enum/order.enum";
+import { IsEnum, IsOptional } from "class-validator";
+import { OrderStatus } from "@prisma/client";
+
+export class OrderPaginationDto extends PaginationDto {
+
+    @IsOptional()
+    @IsEnum( OrderStatusList, {
+        message: `Valid status are ${ OrderStatusList }`
+    })
+    status: OrderStatus;
+}

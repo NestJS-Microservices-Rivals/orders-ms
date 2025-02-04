3e7e8b9d424ae0b01c017c5aa734e839a3ef24d9
yvromero <yvonne.romero@uds.edu.py>
2025-02-04 19:45:30 -0300

    Update orders: improvements in DTO, service, and Prisma schema

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,16 +1,23 @@
 import { Controller, ParseUUIDPipe } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { ChangeOrderStatusDto, OrderPaginationDto } from './dto';
+import { ChangeOrderStatusDto, OrderPaginationDto, PaidOrderDto } from './dto';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @MessagePattern('createOrder')
-  create(@Payload() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  async create(@Payload() createOrderDto: CreateOrderDto) {
+
+    const order = await this.ordersService.create(createOrderDto);
+    const paymentSession = await this.ordersService.createPaymentSession(order);
+
+    return {
+      order,
+      paymentSession
+    }
   }
 
   @MessagePattern('findAllOrders')
@@ -26,5 +33,10 @@ export class OrdersController {
   @MessagePattern('changeOrderStatus')
   changeStatus(@Payload() changeOrderStatusDto: ChangeOrderStatusDto ) {
     return this.ordersService.changeStatus(changeOrderStatusDto);
+  }
+
+  @EventPattern('payment.succeeded')
+  paidOrder(@Payload() paidOrderDto: PaidOrderDto ) {
+    return this.ordersService.paidOrder( paidOrderDto );
   }
 }
